147914ce396ddb48e94db6857dbaa34bb0165def
Stephen Hebert <stephen@goteamup.com>
2022-04-27 12:24:32 -0500

    simplify vite config

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,7 @@ import Unocss from 'unocss/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  mode: 'production',
   build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/main.js'),
-      name: 'App',
-      formats: ['es'],
-      fileName: (format) => `App.${format}.js`
-    },
-    target: 'es2015',
     sourcemap: true,
   },
   plugins: [
